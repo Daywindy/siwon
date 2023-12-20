2c95c2e1065171e77edd252a507a38d114534073
min680 <jeongmin6804@gmail.com>
2023-12-20 22:33:42 +0900

    2023591031

--- app/src/main/java/com/tu/siwon/MainActivity.kt
+++ app/src/main/java/com/tu/siwon/MainActivity.kt
@@ -1,27 +1,18 @@
+package com.tu.siwon
+import com.tu.siwon.R
 
-package com.tu.swion
-
-pacakage com.tu.swion
-ipmport android.graphics.Color
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -38,6 +29,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 지도 프래그먼트 초기화 및 레이아웃에 추가
         mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
@@ -47,32 +39,43 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         placeListView = findViewById(R.id.placeListView)
         buttonsContainer = findViewById(R.id.buttonsContainer)
 
+        // 가족 코스 추천 버튼 클릭 시
         val showFamilyCourseButton: Button = findViewById(R.id.showFamilyCourseButton)
         showFamilyCourseButton.setOnClickListener {
             showPlacesOnMapWithLines(
                 listOf(
-                    Place("시흥갯골생태공원", "자연명소", 37.3895, 126.7808),
-                    // ... (add more places as needed)
+                    Place("연꽃테마파크", "가족코스", 37.3836, 126.7224),
+                    Place("갯골생태공원", "가족코스", 37.3895, 126.7808),
+                    Place("오이도", "가족코스", 37.3791, 126.7375),
+                    Place("오이도선사유적공원", "가족코스", 37.3793, 126.7456),
+                    Place("하늘휴게소", "가족코스", 37.3847, 126.7505),
+                    Place("시흥관광호텔핀", "가족코스", 37.3797, 126.7341)
                 )
             )
         }
 
+        // 연인 데이트 코스 추천 버튼 클릭 시
         val showDateCourseButton: Button = findViewById(R.id.showDateCourseButton)
         showDateCourseButton.setOnClickListener {
             showPlacesOnMapWithLines(
                 listOf(
                     Place("놀숲 시흥은행점", "연인데이트코스", 37.3734, 126.7339),
-                    // ... (add more places as needed)
+                    Place("월곶예술공판장 아트독", "연인데이트코스", 37.3877, 126.7429),
+                    Place("시흥 프리미엄 아울렛", "연인데이트코스", 37.3803, 126.7293),
+                    Place("오이도 빨강등대", "연인데이트코스", 37.3814, 126.7348)
                 )
             )
         }
 
+        // 아동 가족 코스 추천 버튼 클릭 시
         val showChildFamilyCourseButton: Button = findViewById(R.id.showChildFamilyCourseButton)
         showChildFamilyCourseButton.setOnClickListener {
             showPlacesOnMapWithLines(
                 listOf(
                     Place("갯골생태공원", "아동가족코스", 37.3895, 126.7808),
-                    // ... (add more places as needed)
+                    Place("용도 수목원", "아동가족코스", 37.3802, 126.7272),
+                    Place("은계호수공원", "아동가족코스", 37.3911, 126.7716),
+                    Place("오이도", "아동가족코스", 37.3791, 126.7375)
                 )
             )
         }
@@ -108,6 +111,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val bounds = boundsBuilder.build()
         mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100, 400, 0))
     }
+
+    // ... (your existing code for location permission, device location, etc.)
 
     private fun checkLocationPermission() {
         // Check for location permission
