2d654d61964e73180fc8139931cf0dbc93e5a994
daywindy <zkxkflsk1023@gmail.com>
2023-12-21 18:00:06 +0900

    2023591031

--- app/DataBaseHelper.java
+++ app/DataBaseHelper.java
@@ -1,94 +1,100 @@
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+import android.content.res.AssetManager;
+import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
-public class DataBaseHelper extends SQLiteOpenHelper
-{
-    // database 의 파일 경로
-    private static String DB_PATH = "";
-    private static String DB_NAME = "siheung_database.db";
-    private Context mContext;
-    private boolean mDataBase;
+public class SQLiteUpDownService {
+    private static SQLiteUpDownService instance;
+    private static final String extDb3FilePath = "databases/siheung_database";
+    private static final String innerDbPath = "data/data/패키지1/databases/siheung_database";
 
-    public DataBaseHelper(Context context)
-    {
-        super(context, DB_NAME, null, 1);
+    private SQLiteUpDownService(){
+        //기본 Constructor입니다.    }
 
-        DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
-        this.mContext = context;
-        dataBaseCheck();
+        public static SQLiteUpDownService getInstance(){
+            if(instance==null){
+                instance = new SQLiteUpDownService();
+            }
+            return instance;
+        }//싱글턴 방식을 사용합니다.
+        //업로드 기능입니다.    //PDA to PC    public void upload(){
+
     }
 
-    private void dataBaseCheck()
-    {
-        File dbFile = new File(DB_PATH + DB_NAME);
 
-        if (!dbFile.exists())
-        {
-            dbCopy();
+        if(checkFile(context)){
+
+        //extDb3File(외부 파일) 을 내부 경로에 복사합니다. (PC to local DB)            File extDb3File = new File(extDb3FilePath);
+
+        File innerDb3File = new File(innerDbPath);
+        if(innerDb3File.exists()==false){
+
+            copyDataBase(context);
         }
     }
 
-    // db를 assets에서 복사해온다.
-    private void dbCopy()
-    {
-        try
-        {
-            File folder = new File(DB_PATH);
-            if (!folder.exists())
-            {
-                folder.mkdir();
+}
+
+    private boolean checkFile(Context context){
+        //파일이 존재할 경우 true를 리턴해주는 파일 존재 확인용 메소드입니다.
+        try {
+
+            InputStream myInput = context.getAssets().open(extDb3FilePath);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+
+    }
+
+    public void copyDataBase(Context context){
+        String PACKAGE_NAME = context.getPackageName();
+
+        AssetManager manager = context.getAssets();
+        String folderPath = "/data/data/" + PACKAGE_NAME + "/databases";
+        String filePath = "/data/data/" + PACKAGE_NAME + "/databases/데이터베이스파일명";
+        File folder = new File(folderPath);
+        File file = new File(filePath);
+
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        try {
+            InputStream is = manager.open("databases/데이터베이스파일명");
+            BufferedInputStream bis = new BufferedInputStream(is);
+
+            if (folder.exists()) {
+            }else{
+                folder.mkdirs();
             }
 
-            InputStream inputStream = mContext.getAssets().open(DB_NAME);
-            String out_filename = DB_PATH + DB_NAME;
-            OutputStream outputStream = new FileOutputStream(out_filename);
-            byte[] mBuffer = new byte[1024];
-            int mLength;
-            while ((mLength = inputStream.read(mBuffer)) > 0)
-            {
-                outputStream.write(mBuffer, 0, mLength);
+            if (file.exists()) {
+                file.delete();
+                file.createNewFile();
             }
-            outputStream.flush();
-            ;
-            outputStream.close();
-            inputStream.close();
 
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+            fos = new FileOutputStream(file);
+            bos = new BufferedOutputStream(fos);
+            int read = -1;
+            byte[] buffer = new byte[1024];
+            while ((read = bis.read(buffer, 0, 1024)) != -1) {
+                bos.write(buffer, 0, read);
+            }
+
+            bos.flush();
+
+            bos.close();
+            fos.close();
+            bis.close();
+            is.close();
+
+        } catch (IOException e) {
+            Log.e("ErrorMessage : ", e.getMessage());
         }
-    }
-
-    @Override
-    public synchronized void close()
-    {
-        if (mDataBase != null)
-        {
-            mDataBase.close();
-        }
-        super.close();
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db)
-    {
-    }
-
-    @Override
-    public void onOpen(SQLiteDatabase db)
-    {
-        super.onOpen(db);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-    {
     }
 }
